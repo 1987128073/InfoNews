e168457e8eb62cf778e10e555b5f65009cbeeb8e
1987128073 <43561034+1987128073@users.noreply.github.com>
2018-12-26 19:37:20 +0800

    flask-session组件保存session数据到redis，配置脚本启动和数据迁移，封装配置信息

---
+++ config.py
@@ -0,0 +1,27 @@
+
+# 定义配置类来封装配置信息
+from datetime import timedelta
+
+from redis import Redis
+
+
+class Config:
+    DEBUG = True  # 设置调试模式
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/infonews"  # 数据库连接地址
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
+    REDIS_HOST = "127.0.0.1"  # redis的连接地址  将自定义的配置也封装到Config类中
+    REDIS_PORT = 6379
+    SESSION_TYPE = "redis"  # 设置session存储的方式  redis 性能好,方便设置过期时间
+    SESSION_REDIS = Redis(host=REDIS_HOST, port=REDIS_PORT)  # 设置Redis连接对象, 组件会使用该对象将session数据保存到redis中
+    SESSION_USE_SIGNER = True  # 设置sessionid进行加密
+    SECRET_KEY = "test"  # 设置sessionid的秘钥
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)  # 设置session过期时间 组件默认支持设置过期时间
+
+
+# 配置信息子类化
+class DevelopmentConfig(Config):  # 开发环境配置信息
+    DEBUG = True  # 设置调试模式
+
+
+class ProductionConfig(Config):  # 生产环境配置信息
+    DEBUG = False
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from redis import Redis
+from config import DevelopmentConfig
 
 app = Flask(__name__)
+mgr = Manager(app)
+# 从对象中加载配置
+app.config.from_object(DevelopmentConfig)
+# 创建msql连接对象
+db = SQLAlchemy(app)
+# 创建redis连接对象
+rs = Redis(host=DevelopmentConfig.REDIS_HOST, port=DevelopmentConfig.REDIS_PORT)
+# 初始化Session对象
+Session(app)
+# 初始化迁移器
+Migrate(app, db)
+# 使用管理器生成迁移命令
+mgr.add_command("mc", MigrateCommand)
 
 
 @app.route('/')
 def index():
-    return 'index'
+    return 'copy '
 
 
 if __name__ == '__main__':
